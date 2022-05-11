4e6b41b24dd9723993531dd59053d31c8a971278
Elladan1337 <kosatka.martin@gmail.com>
2022-05-11 22:20:57 +0200

    Edited some language, added comments.

--- addressPaster.py
+++ addressPaster.py
@@ -4,14 +4,14 @@
 # It is forbidden to make more than 50 requests per minute, or 3000 requests per day.
 # This script is distributed under a AGPL-3.0 license.
 
-
+# Importing the necessary libraries
 import requests
 from lxml import etree
 from bs4 import BeautifulSoup
 
 
+# Creates the correct URL based on whether a number or name was inputted.
 def url_creator(identifier):
-
     if identifier.isnumeric():  # ICO
         url = ('https://or.justice.cz/ias/ui/rejstrik-$firma?p%3A%3Asubmit=x&.%2Frejstrik-%24firma=&nazev=&ico=' +
                str(identifier) +
@@ -25,13 +25,15 @@ def url_creator(identifier):
     return url
 
 
+# Requests the website, I am not entirely sure what happens here, but it works.
 def retrieve(url):
     webpage = requests.get(url)
     webpage = BeautifulSoup(webpage.content, 'html.parser')
-    webpage = etree.HTML(str(webpage))
+    webpage = etree.HTML(str(webpage))                      # etree is used because BS4 doesn't support XPath
     return webpage
 
 
+# Uses XPath to return a dictionary of the relevant information.
 def credentialize(webpage):
     credentials = {'address': webpage.xpath('//*[@id="idd"]/div/span/span')[0].text,
                    'name': webpage.xpath('//*[@id="SearchResults"]/div[2]/div/ol/li/div/'
@@ -45,6 +47,7 @@ def credentialize(webpage):
     return credentials
 
 
+# Pastes the relevant information into a format commonly used by lawyers.
 def outputter(credentials):
     output = 'Společnost ' + credentials['name'] + ' se sídlem na adrese ' + credentials['address'] + ' IČ: ' + \
              credentials['ico'] + '.'
@@ -52,7 +55,7 @@ def outputter(credentials):
 
 
 def main():
-    print("Input company name or identification number")
+    print("Enter company name or identification number")
     identifier = input()
     print(outputter(credentialize(retrieve(url_creator(identifier)))))
 
